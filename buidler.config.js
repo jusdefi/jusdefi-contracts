9956c406b16384540ef624aa47ebd29afefa41c5
Nick Barry <itsnickbarry@protonmail.ch>
2020-10-17 18:29:25 -0500

    set test timeout required for ganache fork mode

--- buidler.config.js
+++ buidler.config.js
@@ -3,6 +3,9 @@ usePlugin('buidler-gas-reporter');
 usePlugin('buidler-spdx-license-identifier');
 usePlugin('solidity-coverage');
 
+// increased timeout required for ganache --fork mode
+const TIMEOUT = 60 * 60 * 1000;
+
 module.exports = {
   solc: {
     version: '0.7.3',
@@ -13,6 +16,9 @@ module.exports = {
   },
 
   networks: {
+    localhost: {
+      timeout: TIMEOUT,
+    },
     generic: {
       // set URL for external network, such as Infura
       url: `${ process.env.URL }`,
@@ -30,5 +36,9 @@ module.exports = {
   spdxLicenseIdentifier: {
     overwrite: false,
     runOnCompile: true,
+  },
+
+  mocha: {
+    timeout: TIMEOUT,
   },
 };
