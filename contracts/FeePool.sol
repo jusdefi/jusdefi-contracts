0fc4ec9a28be4e72977679be195d32217bb92e84
Nick Barry <itsnickbarry@protonmail.ch>
2020-10-26 20:40:41 -0600

    remove console.sol import

--- contracts/FeePool.sol
+++ contracts/FeePool.sol
@@ -7,8 +7,6 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 
-import '@nomiclabs/buidler/console.sol';
-
 import './interfaces/IJusDeFi.sol';
 import './interfaces/IStakingPool.sol';
 
