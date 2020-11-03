91e9938ec64c536da9cb4456ba04c63cfe3ee50b
Nick Barry <itsnickbarry@protonmail.ch>
2020-11-02 19:29:52 -0600

    add airdrop script

---
+++ scripts/airdrop.js
@@ -0,0 +1,24 @@
+const {
+  airdropToken,
+} = require('../data/deployments.json');
+
+const airdrops = require('airdrop-ledger/airdrops.json');
+
+const AirdropToken = artifacts.require('AirdropToken');
+
+async function main() {
+  const instance = await AirdropToken.at(airdropToken);
+
+  const accounts = airdrops.map(d => d[0]);
+  const amounts = airdrops.map(d => d[1]);
+
+  let tx = await instance.airdrop(accounts, amounts);
+  console.log(tx.receipt.gasUsed);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
